adbc3e3cae6492f78dcde90e7e4830ebca10f329
zombow <82148187+zombow@users.noreply.github.com>
2025-06-28 00:46:27 +0900

    [06/28] Inventory PopUp 제작
    남은 UI
    상점, 전투, 알림,  스테이트 (스테이트 상승)

--- Assets/ProjectFile/Scripts/UI/FullpitImage.cs
+++ Assets/ProjectFile/Scripts/UI/FullpitImage.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class FullpitImage : MonoBehaviour
 {
     public Image image; // Preserved Aspect가 켜진 이미지
     public RectTransform fitPanel; // 이 패널이 '실제 이미지 영역'을 커버
 
     void Start()
+    {
+        UpdateFitPanel();
+    }
+
+    void UpdateFitPanel()
     {
         RectTransform imgRect = image.rectTransform;
         Rect spriteRect = image.sprite.rect;
@@ -37,4 +46,23 @@ public class FullpitImage : MonoBehaviour
         fitPanel.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, fitHeight);
         fitPanel.anchoredPosition = Vector2.zero; // 이미지 중앙
     }
+#if UNITY_EDITOR
+    [CustomEditor(typeof(FullpitImage))]
+    public class FullpitImageEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            FullpitImage script = (FullpitImage)target;
+
+            if (GUILayout.Button("이미지 크기에 맞춰 패널 조정"))
+            {
+                script.UpdateFitPanel();
+                // SceneView 갱신
+                EditorUtility.SetDirty(script);
+            }
+        }
+    }
+#endif
 }
